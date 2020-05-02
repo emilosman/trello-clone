634568dee50360836477f80fbee8f221e6186aac
Emil Osmanbegovic <emilosmanbegovic@gmail.com>
2020-05-02 23:34:51 +0200

    add icon and percentage

--- app/javascript/packs/components/card-checklist.vue
+++ app/javascript/packs/components/card-checklist.vue
@@ -2,7 +2,7 @@
   <div>
     <h6>Checklist</h6>
     <div class="progress mb-2">
-      <div class="progress-bar" role="progressbar" v-bind:style="'width:' + progressValue +'%'" v-bind:class="{'bg-success': progressValue == 100}"></div>
+      <div class="progress-bar" role="progressbar" v-bind:style="'width:' + progressValue +'%'" v-bind:class="{'bg-success': progressValue == 100}">{{progressValue}}%</div>
     </div>
     <checklist-item :checklistItems="checklist.items" v-on:change="$emit('change')"></checklist-item>
     <button class="btn btn-light" @click="addChecklistItem">
